bb00de04ff900fa7da7b72d55ff8323565b0a88e
TrooperCrypto <trooper.crypto7@gmail.com>
2022-11-03 23:54:25 +0100

    fix eip1271 implementation

--- contracts/contracts/Vault.sol
+++ contracts/contracts/Vault.sol
@@ -1,5 +1,5 @@
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {SignatureChecker} from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract Vault is ERC20 {
   // The manager of a vault is allowed to sign orders that a vault can execute
@@ -28,12 +28,19 @@ contract Vault is ERC20 {
     _burn(address(this), amount);
   }
 
-
   ////////////////////////////////////////////////////
   // EIP-1271 Smart Contract Signatures
+  // https://eips.ethereum.org/EIPS/eip-1271
 
-  function isValidSignature(bytes32 digest, bytes memory signature) public view returns (bool) {
-    return SignatureChecker.isValidSignatureNow(manager, digest, signature);
+  function isValidSignature(
+    bytes32 _hash,
+    bytes calldata _signature
+  ) external override view returns (bytes4) {
+    (address recovered, ECDSA.RecoverError error) = ECDSA.tryRecover(_hash, _signature);
+    if (error == ECDSA.RecoverError.NoError && recovered == manager) {
+      return 0x1626ba7e;
+    } else {
+      return 0xffffffff;
+    }
   }
-
 }
