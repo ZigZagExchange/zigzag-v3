fd0e9e7fce0c8a06071dd9b6f281792b2389f32e
taureau75 <vixidev99@protonmail.com>
2022-12-10 20:56:38 -0800

    markets route

--- src/routes/markets.ts
+++ src/routes/markets.ts
@@ -1,10 +1,25 @@
 import type {
   ZZHttpServer,
 } from '../types'
+import { db } from '../db';
 
 export default function marketRoutes(app: ZZHttpServer) {
 
   app.get('/v1/markets', async (req, res) => {
+    const selectDistinctMarkets = await db.query("SELECT DISTINCT(buyToken, sellToken) FROM orders");
+    const selectTokens = await db.query("SELECT * FROM tokeninfo");
+
+    // Mark verified markets
+    const verifiedTokenAddresses = selectTokens.rows.map(r => r.token_address);
+    const markets = selectDistinctMarkets.rows.map(row => ({
+      ...row,
+      verified: (verifiedTokenAddresses.includes(row.buyToken) && verifiedTokenAddresses.includes(row.sellToken)),
+    }));
+
+    const result = {
+      markets,
+      verifiedTokens: selectTokens.rows,
+    }
   })
 
 }
